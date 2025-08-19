edf099a67f4e2f9413aeb50b5127c19935807e57
fate1997 <rengp97@gmail.com>
2025-08-19 12:03:53 +0100

    use absolute path in the scripts

--- scripts/run_dl.sh
+++ scripts/run_dl.sh
@@ -1,8 +1,12 @@
 MODEL=$1
 TAG=$2
 
-python main_dl.py --model $MODEL --raw-csv ../database/external/Tg_merged.csv --labels Tg --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250
-python main_dl.py --model $MODEL --raw-csv ../database/external/FFV_merged.csv --labels FFV --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250
-python main_dl.py --num-epochs 1000 --model $MODEL --raw-csv ../database/external/Tc_merged.csv --labels Tc --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250
-python main_dl.py --num-epochs 1000 --model $MODEL --raw-csv ../database/external/Rg_relative_difference_merged.csv --labels Rg --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250
-python main_dl.py --num-epochs 1000 --model $MODEL --raw-csv ../database/internal/train.csv  --labels Density --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250
+# Get the directory of this script
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $SCRIPT_DIR
+
+python "$SCRIPT_DIR/main_dl.py" --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/Tg_merged.csv" --labels Tg --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250
+python "$SCRIPT_DIR/main_dl.py" --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/FFV_merged.csv" --labels FFV --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250
+python "$SCRIPT_DIR/main_dl.py" --num-epochs 1000 --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/Tc_merged.csv" --labels Tc --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250
+python "$SCRIPT_DIR/main_dl.py" --num-epochs 1000 --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/Rg_relative_difference_merged.csv" --labels Rg --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250
+python "$SCRIPT_DIR/main_dl.py" --num-epochs 1000 --model $MODEL --raw-csv "$SCRIPT_DIR/../database/internal/train.csv"  --labels Density --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250
--- scripts/run_ml.sh
+++ scripts/run_ml.sh
@@ -1,8 +1,12 @@
 MODEL=$1
 TAG=$2
 
-python main_ml.py --model $MODEL --raw-csv ../database/external/Tg_merged.csv --labels Tg --tag $TAG --optimize-hparams
-python main_ml.py --model $MODEL --raw-csv ../database/external/FFV_merged.csv --labels FFV --tag $TAG --optimize-hparams
-python main_ml.py --model $MODEL --raw-csv ../database/external/Tc_merged.csv --labels Tc --tag $TAG --optimize-hparams
-python main_ml.py --model $MODEL --raw-csv ../database/external/Rg_relative_difference_merged.csv --labels Rg --tag $TAG --optimize-hparams
-python main_ml.py --model $MODEL --raw-csv ../database/internal/train.csv  --labels Density --tag $TAG --optimize-hparams
+# Get the directory of this script
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $SCRIPT_DIR
+
+python "$SCRIPT_DIR/main_ml.py" --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/Tg_merged.csv" --labels Tg --tag $TAG --optimize-hparams
+python "$SCRIPT_DIR/main_ml.py" --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/FFV_merged.csv" --labels FFV --tag $TAG --optimize-hparams
+python "$SCRIPT_DIR/main_ml.py" --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/Tc_merged.csv" --labels Tc --tag $TAG --optimize-hparams
+python "$SCRIPT_DIR/main_ml.py" --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/Rg_relative_difference_merged.csv" --labels Rg --tag $TAG --optimize-hparams
+python "$SCRIPT_DIR/main_ml.py" --model $MODEL --raw-csv "$SCRIPT_DIR/../database/internal/train.csv"  --labels Density --tag $TAG --optimize-hparams
