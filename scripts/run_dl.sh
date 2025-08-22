4ee30079fce65f00029c511bfffc3379bd686719
fate1997 <rengp97@gmail.com>
2025-08-22 17:01:24 +0100

    update results and version

--- scripts/run_dl.sh
+++ scripts/run_dl.sh
@@ -1,12 +1,13 @@
 MODEL=$1
 TAG=$2
+N_TRIALS=${3:-25}
 
 # Get the directory of this script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $SCRIPT_DIR
 
-python "$SCRIPT_DIR/main_dl.py" --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/Tg_merged.csv" --labels Tg --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250
-python "$SCRIPT_DIR/main_dl.py" --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/FFV_merged.csv" --labels FFV --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250
-python "$SCRIPT_DIR/main_dl.py" --num-epochs 1000 --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/Tc_merged.csv" --labels Tc --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250
-python "$SCRIPT_DIR/main_dl.py" --num-epochs 1000 --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/Rg_relative_difference_merged.csv" --labels Rg --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250
-python "$SCRIPT_DIR/main_dl.py" --num-epochs 1000 --model $MODEL --raw-csv "$SCRIPT_DIR/../database/internal/train.csv"  --labels Density --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250
+python "$SCRIPT_DIR/main_dl.py" --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/Tg_merged.csv" --labels Tg --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250 --n-trials $N_TRIALS
+python "$SCRIPT_DIR/main_dl.py" --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/FFV_merged.csv" --labels FFV --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250 --n-trials $N_TRIALS
+python "$SCRIPT_DIR/main_dl.py" --num-epochs 1000 --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/Tc_merged.csv" --labels Tc --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250 --n-trials $N_TRIALS
+python "$SCRIPT_DIR/main_dl.py" --num-epochs 1000 --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/Rg_relative_difference_merged.csv" --labels Rg --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250 --n-trials $N_TRIALS
+python "$SCRIPT_DIR/main_dl.py" --num-epochs 1000 --model $MODEL --raw-csv "$SCRIPT_DIR/../database/internal/train.csv"  --labels Density --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250 --n-trials $N_TRIALS
