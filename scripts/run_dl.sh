d279e610d6fe15704a87a7095ef2143c9d9a550c
fate1997 <rengp97@gmail.com>
2025-08-25 21:45:03 +0100

    add additional arguments

--- scripts/run_dl.sh
+++ scripts/run_dl.sh
@@ -2,16 +2,17 @@ MODEL=$1
 TAG=$2
 N_TRIALS=${3:-25}
 N_FOLD=${4:-1}
+ADDI_ARGS=${5:-""}
 
 # Get the directory of this script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $SCRIPT_DIR
 echo "Number of trials: $N_TRIALS"
 echo "Number of folds: $N_FOLD"
+echo "Additional arguments: $ADDI_ARGS"
 
-
-python "$SCRIPT_DIR/main_dl.py" --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/Tg_merged.csv" --labels Tg --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250 --n-trials $N_TRIALS --n-fold $N_FOLD
-python "$SCRIPT_DIR/main_dl.py" --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/FFV_merged.csv" --labels FFV --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250 --n-trials $N_TRIALS --n-fold $N_FOLD
-python "$SCRIPT_DIR/main_dl.py" --num-epochs 1000 --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/Tc_merged.csv" --labels Tc --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250 --n-trials $N_TRIALS --n-fold $N_FOLD
-python "$SCRIPT_DIR/main_dl.py" --num-epochs 1000 --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/Rg_relative_difference_merged.csv" --labels Rg --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250 --n-trials $N_TRIALS --n-fold $N_FOLD
-python "$SCRIPT_DIR/main_dl.py" --num-epochs 1000 --model $MODEL --raw-csv "$SCRIPT_DIR/../database/internal/train.csv"  --labels Density --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250 --n-trials $N_TRIALS --n-fold $N_FOLD
+python "$SCRIPT_DIR/main_dl.py" --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/Tg_merged.csv" --labels Tg --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250 --n-trials $N_TRIALS --n-fold $N_FOLD $ADDI_ARGS
+python "$SCRIPT_DIR/main_dl.py" --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/FFV_merged.csv" --labels FFV --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250 --n-trials $N_TRIALS --n-fold $N_FOLD $ADDI_ARGS
+python "$SCRIPT_DIR/main_dl.py" --num-epochs 1000 --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/Tc_merged.csv" --labels Tc --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250 --n-trials $N_TRIALS --n-fold $N_FOLD $ADDI_ARGS
+python "$SCRIPT_DIR/main_dl.py" --num-epochs 1000 --model $MODEL --raw-csv "$SCRIPT_DIR/../database/external/Rg_relative_difference_merged.csv" --labels Rg --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250 --n-trials $N_TRIALS --n-fold $N_FOLD $ADDI_ARGS
+python "$SCRIPT_DIR/main_dl.py" --num-epochs 1000 --model $MODEL --raw-csv "$SCRIPT_DIR/../database/internal/train.csv"  --labels Density --run-production --optimize-hparams --tag $TAG --early-stopping-patience 250 --n-trials $N_TRIALS --n-fold $N_FOLD $ADDI_ARGS
